1288110b117b7e253b14f32d84a862dc01987ba9
EvieMahsem <106112473+EvieMahsem@users.noreply.github.com>
2022-07-08 08:26:16 -0500

    cleaned spark functions

--- spark_functions.py
+++ spark_functions.py
@@ -1,15 +1,11 @@
-
-
 class SparkQuerrying():
     def __init__(self,df,colInput):
         self.df = df
         self.colInput = colInput
-
     def SelectWhereGBy(self,whereCon,groupByVar,aggColnName,aggType):
         aggreagteDict = {k:v for (k,v) in zip(aggColnName, aggType)}
         try:
             self.df.select(self.colInput).where(f'{whereCon}').groupby(f'{groupByVar}').agg(aggreagteDict).show()
-
         except Exception as e:
             print(e)        
     def SelectGB(self,groupByVar,aggColnName,aggType):
@@ -28,13 +24,3 @@ class SparkQuerrying():
             self.df.select(self.colInput).show()
         except Exception as e:
             print(e)  
-
-
-
-class DataAnalysis(SparkQuerrying):
-    def __init__(self):
-        pass
-    def plotData(self):
-        pass
-
-
