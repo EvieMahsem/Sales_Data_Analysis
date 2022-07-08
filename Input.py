17f548653014c7224bf5e4b4200af86e1bd45cfc
EvieMahsem <106112473+EvieMahsem@users.noreply.github.com>
2022-07-08 08:25:51 -0500

    Fixed up the menu a little

--- Input.py
+++ Input.py
@@ -9,51 +9,48 @@ spark_df = spark.createDataFrame(data)
 
 def insert_query():
     while True:
-        print("This is the querying portion of the program.")
-        lel = 0
-        while lel != 5:
-            print("\t1. To do a Where statment AND a Group By")
-            print("\t2. To do a Where statment AND NO group By")
-            print("\t3. To do a select statement with just a Group By") 
-            print("\t4. A select statement without a where or group by")
-            print("\t5. To return to the main menu")
-            lel = int(input("Please make a selection: "))
-            print("How many columns do you want to select?")
-            print(f"The columns you can select are: {spark_df.columns}")
-            colInput = input("Please list the columns you would like: ").split(',')
-            obj = SparkQuerrying(spark_df,colInput) 
-            aggreagteFunctions = ['count','max','min','first','last', 'mean','sum']
-        
-            if lel == 1:
-                print(colInput)
-                print("What is the conditional you would like to use? For example: orderID > 10 ")
-                whereCon = input("Please input: ")
-                print("What column do you want to group by?", colInput)
-                groupByVar = input("Please input: ")
-                keys = input("Please input the column names you want to aggregate: ").split(',')
-                print(aggreagteFunctions)
-                values = input("Please input the functions you would like to use: ").split(',')
-                obj.SelectWhereGBy(whereCon,groupByVar,keys,values)
-            elif lel == 2:
-                print(colInput)
-                print("What is the conditional you would like to use? For example: orderID > 10 ")
-                whereCon = input("Please input: ")
-                obj.SelectWhere(whereCon)                    
-            elif lel == 3:
-                print(colInput)
-                print("What column do you want to group by?", colInput)
-                groupByVar = input("Please input: ")
-                keys = input("Please input the column names you want to aggreagte: ").split(',')
-                print(aggreagteFunctions)
-                values = input("Please input the functions you would like to use: ").split(',')
-                obj.SelectGBy(groupByVar,keys,values)
-            elif lel == 4:
-                print(colInput)
-                obj.Select(colInput)
-            elif lel == 5:
-                break
-            else:
-                print("Please make a valid input.")
+        print("\t1. To do a Where statment AND a Group By")
+        print("\t2. To do a Where statment AND NO group By")
+        print("\t3. To do a select statement with just a Group By") 
+        print("\t4. A select statement without a where or group by")
+        print("\t5. To return to the main menu")
+        lel = int(input("Please make a selection: "))
+        if lel == 5:
+            break
+        print("How many columns do you want to select?")
+        print(f"The columns you can select are: {spark_df.columns}")
+        colInput = input("Please list the columns you would like: ").split(',')
+        obj = SparkQuerrying(spark_df,colInput) 
+        aggreagteFunctions = ['count','max','min','first','last', 'mean','sum']
+    
+        if lel == 1:
+            print(colInput)
+            print("What is the conditional you would like to use? For example: orderID > 10 ")
+            whereCon = input("Please input: ")
+            print("What column do you want to group by?", colInput)
+            groupByVar = input("Please input: ")
+            keys = input("Please input the column names you want to aggregate: ").split(',')
+            print(aggreagteFunctions)
+            values = input("Please input the functions you would like to use: ").split(',')
+            obj.SelectWhereGBy(whereCon,groupByVar,keys,values)
+        elif lel == 2:
+            print(colInput)
+            print("What is the conditional you would like to use? For example: orderID > 10 ")
+            whereCon = input("Please input: ")
+            obj.SelectWhere(whereCon)                    
+        elif lel == 3:
+            print(colInput)
+            print("What column do you want to group by?", colInput)
+            groupByVar = input("Please input: ")
+            keys = input("Please input the column names you want to aggreagte: ").split(',')
+            print(aggreagteFunctions)
+            values = input("Please input the functions you would like to use: ").split(',')
+            obj.SelectGBy(groupByVar,keys,values)
+        elif lel == 4:
+            print(colInput)
+            obj.Select(colInput)
+        else:
+            print("Please make a valid input.")
 
 def Startup ():
     while True:
